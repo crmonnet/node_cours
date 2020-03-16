a6f5b7016d4bed901cb6580e9006b527c1737e18
CyrilRM <cyril.rolland.monnet2e1@gmail.com>
2020-03-16 15:00:37 +0100

    Ajout du port 3000

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Connexion réussie !');
 });
 
