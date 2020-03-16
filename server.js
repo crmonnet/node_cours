87dd4ce5fa5b30f4e145f919fe2ff63e955c121f
CyrilRM <cyril.rolland.monnet2e1@gmail.com>
2020-03-16 17:11:18 +0100

    Ajout des options météo et ville

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.json())
+
 app.get('/', function (req, res) {
 	res.send('Hello World!\n');
 });
-
 
 app.get('/hello', function(req, res){
 	nom = req.query.nom;
@@ -17,8 +19,21 @@ app.get('/hello', function(req, res){
 	}
 });
 
+app.post('/chat', function(req, res) {
+	var msg = req.body.msg;
+	console.log(req.body);
+	console.log(msg);
+
+	if (msg == "ville") {
+		res.send("Nous sommes à Paris\n");
+	}else if(msg == "météo") {
+		res.send("Il fait beau\n");
+	}else{
+		res.send("Merci de faire un requete correcte !");
+	}
+});
+
 app.listen(port, function () {
 	console.log('Connexion réussie !');
 	console.log('Port : ' + port);
 });
-
